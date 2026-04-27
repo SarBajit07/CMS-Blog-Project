5a0c316aba29990983b92bed5d1f79978d0b77e2
SarBajit07 <104916170+SarBajit07@users.noreply.github.com>
2026-04-27 09:55:55 +0545

    chore: text editor error fixed

--- frontend/src/components/dashboard/RichTextEditor.tsx
+++ frontend/src/components/dashboard/RichTextEditor.tsx
@@ -200,6 +200,7 @@ export default function RichTextEditor({ value, onChange, placeholder }: RichTex
       }),
     ],
     content: value,
+    immediatelyRender: false,
     onUpdate: ({ editor }) => {
       onChange(editor.getHTML());
     },
